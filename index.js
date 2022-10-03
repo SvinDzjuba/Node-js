9daeb3b6257c8d31242573f59bda97c08900c409
Maksim Dzjubenko <90320393+SvinDzjuba@users.noreply.github.com>
2022-10-03 10:10:42 +0300

    Worked with post, put, delete

--- index.js
+++ index.js
@@ -1,5 +1,6 @@
 const express = require('express');
 const app = express();
+app.use(express.json());
 const db = require('./config');
 
 app.get('/films', function (req, res) {
@@ -52,6 +53,100 @@ app.get('/genre-films', function (req, res) {
     db.query('SELECT name, COUNT(F.film_id) as counter FROM category as C inner join film_category as FC on C.category_id = FC.category_id inner join film as F on FC.film_id = F.film_id GROUP BY name', function (error, results) {
         if (error) throw error;
         return res.send({data: results});
+    });
+});
+
+// =======================
+
+// ACTOR
+// POST
+app.post('/add-actor', (req, res) => {
+    db.query(`INSERT INTO actor SET ?`, req.body, (error, results) => {
+        // {
+        //     "first_name": "",
+        //     "last_name": "",
+        // }
+        if (error) throw error;
+        res.status(201).send(`Actor added with ID: ${results.insertId}`);
+    });
+});
+// UPDATE
+app.put('/update-actor/:id', (req, res) => {
+    db.query(`UPDATE actor SET ? WHERE actor_id = ?`, [req.body, req.params.id], (error, results) => {
+        if (error) throw error;
+        res.status(200).send(`Actor updated with ID: ${req.params.id}`);
+    });
+});
+// DELETE
+app.delete('/delete-actor/:id', (req, res) => {
+    db.query(`DELETE FROM actor WHERE actor_id = ?`, req.params.id, (error, results) => {
+        if (error) throw error;
+        res.status(200).send(`Actor deleted with ID: ${req.params.id}`);
+    });
+});
+
+
+// -----------
+// FILM
+// POST
+app.post('/add-film', (req, res) => {
+    db.query(`INSERT INTO film SET ?`, req.body, (error, results) => {
+        // {
+        //     "title": "",
+        //     "description": "",
+        //     "release_year": ,
+        //     "language_id": ,
+        //     "rental_duration": ,
+        //     "rental_rate": ,
+        //     "length": ,
+        //     "replacement_cost": ,
+        //     "rating": "",
+        //     "special_features": ""
+        // }
+        if (error) throw error;
+        res.status(201).send(`Film added with ID: ${results.insertId}`);
+    });
+});
+// UPDATE
+app.put('/update-film/:id', (req, res) => {
+    db.query(`UPDATE film SET ? WHERE film_id = ?`, [req.body, req.params.id], (error, results) => {
+        if (error) throw error;
+        res.status(200).send(`Film updated with ID: ${req.params.id}`);
+    });
+});
+// DELETE
+app.delete('/delete-film/:id', (req, res) => {
+    db.query(`DELETE FROM film WHERE film_id = ?`, req.params.id, (error, results) => {
+        if (error) throw error;
+        res.status(200).send(`Film deleted with ID: ${req.params.id}`);
+    });
+});
+
+
+// -----------
+// CATEGORY
+// POST
+app.post('/add-category', (req, res) => {
+    db.query(`INSERT INTO category SET ?`, req.body, (error, results) => {
+        // {
+        //     "name": "",
+        // }
+        if (error) throw error;
+        res.status(201).send(`Category added with ID: ${results.insertId}`);
+    });
+});
+// UPDATE
+app.put('/update-category/:id', (req, res) => {
+    db.query(`UPDATE category SET ? WHERE category_id = ?`, [req.body, req.params.id], (error, results) => {
+        if (error) throw error;
+        res.status(200).send(`Category updated with ID: ${req.params.id}`);
+    });
+});
+// DELETE
+app.delete('/delete-category/:id', (req, res) => {
+    db.query(`DELETE FROM category WHERE category_id = ?`, req.params.id, (error, results) => {
+        if (error) throw error;
+        res.status(200).send(`Category deleted with ID: ${req.params.id}`);
     });
 });
 
